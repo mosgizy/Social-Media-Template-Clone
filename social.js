63f99b485c1c321f240b0167342de1c03575df87
Moshood Ope <Moshood521@gmail.com>
2021-04-15 12:53:19 +0100

    added clicking and info to the groups

--- social.js
+++ social.js
@@ -1,3 +1,16 @@
 function closeFunc(){
 document.getElementById("close").style.display="none";
 }
+
+    /*    document.getElementsByClassName("drop-content" + val).classList.toggle("hide");*/
+function toggle(val){
+    let hold = document.getElementsByClassName("drop-content");
+    
+    if(hold[val].style.display == 'inline-grid'){
+	hold[val].style.display = 'none';
+    }else{
+	hold[val].style.display = 'inline-grid';
+    }
+}
+
+
