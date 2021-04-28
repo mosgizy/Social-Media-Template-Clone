0757bb8cf91c1f8b1a471794a24a31bd26c71526
Moshood Ope <Moshood521@gmail.com>
2021-04-28 14:42:01 +0100

    removed the inline event listener and refactor the JS code

--- social.js
+++ social.js
@@ -1,16 +1,28 @@
-function closeFunc(){
-document.getElementById("close").style.display="none";
-}
-
-    /*    document.getElementsByClassName("drop-content" + val).classList.toggle("hide");*/
-function toggle(val){
-    let hold = document.getElementsByClassName("drop-content");
-    
-    if(hold[val].style.display == 'inline-grid'){
-	hold[val].style.display = 'none';
-    }else{
-	hold[val].style.display = 'inline-grid';
+window.onload = () => {
+    function closeFunc(){
+	document.getElementById("close").style.display="none";
     }
+    
+    let hold = document.getElementsByClassName("drop-content");
+    const content = document.querySelectorAll('.sub-content');
+    
+    function toggle(val){
+	if(hold[val].style.display == 'inline-grid'){
+	    hold[val].style.display = 'none';
+	}else{
+	    hold[val].style.display = 'inline-grid';
+	}
+    }
+    
+    let count = hold.length,temp=0;
+    
+    for(let i = 0; i<content.length;i++){
+	content[i].onclick = function(){
+	    toggle(i);
+	}
+	
+    }
+
 }
 
 
